4b9619a6fb018452f2d2df8f6875616b7a719b1c
yangjunlin <519100949@qq.com>
2015-12-17 11:04:42 +0800

    add PathGest.java and splitTest.java

--- JavaBase/src/main/java/com/yjl/javabase/stringT/SplitTest.java
+++ JavaBase/src/main/java/com/yjl/javabase/stringT/SplitTest.java
@@ -3,7 +3,7 @@ package com.yjl.javabase.stringT;
 /**
  * Created by on 12/17/15.
  */
-public class splitTest {
+public class SplitTest {
     public static void main(String[] args) {
         String string = ".test.buaa";
         String[] strings = string.split("\\.");
